b89688a89e614fc1cf7c3337f9bb414f19d7aa14
MeDiiNa2 <dmedina@inscaparrella.cat>
2022-06-09 11:38:42 +0200

    1r commit constructors

---
+++ GitJunit.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+class GitJunit {
+
+	@Test
+	void test() {
+		 Scanner entrada = new Scanner(System.in);  
+	     System.out.println("Introdueix numero de felicitat"); 
+	     int F1 = entrada.nextInt();
+	     int F2 = entrada.nextInt();
+	     int F3 = entrada.nextInt();
+	     if(F1<F2 && F2<F3 && F1<F3) {
+	    	 System.out.println("Si");
+	     }else {
+	    	 System.out.println("No");
+	     }
+		
+	}
+
+}
